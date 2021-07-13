af4a58aa608952b14191362336f499318a0f8a01
Michiel de Jong <michiel@unhosted.org>
2021-07-13 10:32:00 +0200

    try with default tomcat conf

--- docker/init.sh
+++ docker/init.sh
@@ -3,7 +3,7 @@
 ## Assumes $DOMAIN is set
 ## Assumes $EMAIL is set
 
-certbot certonly --standalone -d $DOMAIN --non-interactive --agree-tos -m=$EMAIL
+# certbot certonly --standalone -d $DOMAIN --non-interactive --agree-tos -m=$EMAIL
 cp /etc/letsencrypt/live/$DOMAIN/privkey.pem /root/tomcat-key.pem
 cp /etc/letsencrypt/live/$DOMAIN/cert.pem /root/tomcat-cert.pem
 cp /etc/letsencrypt/live/$DOMAIN/chain.pem /root/tomcat-rsa-chain.pem
